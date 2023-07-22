541b6941739fb4097227eb56384bcba3045733b5
Malu <mndzebelemalungisa@gmail.com>
2023-07-21 21:06:29 -0500

    idk :(

--- project/app/main.py
+++ project/app/main.py
@@ -15,7 +15,7 @@ register_tortoise(
     app,
     db_url=os.environ.get("DATABASE_URL"),
     modules={"models": ["app.models.tortoise"]},
-    generate_schemas=True,
+    generate_schemas=False,
     add_exception_handlers=True,
 )
 
@@ -25,5 +25,6 @@ async def pong(settings: Settings = Depends(get_settings)):
     return {
         "ping": "pong!",
         "environment": settings.environment,
-        "testing": settings.testing
+        "testing": settings.testing,
+        "database_url":settings.database_url
     }
