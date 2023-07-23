1e8b2f8e5a3dab9c9656f16fdf9fe8bd8e2d6ba1
Malu <mndzebelemalungisa@gmail.com>
2023-07-23 14:52:31 -0500

    RESTful Routes - complete

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -2,6 +2,7 @@
 
 
 from fastapi import APIRouter, HTTPException
+from typing import List
 
 from app.api import crud
 from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
@@ -27,4 +28,8 @@ async def read_summary(id: int) -> SummarySchema:
         raise HTTPException(status_code=404, detail="Summary not found")
 
     return summary
+
+@router.get("/", response_model=List[SummarySchema])
+async def read_all_summaries() -> List[SummarySchema]:
+    return await crud.get_all()
 
